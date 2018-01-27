bfbca764f6b795a05d46679279ee0f3d71acdb3c
Robert Barnes <robertbarnes1981@googlemail.com>
2018-01-27 21:28:25 +0000

    Test charting code

--- pyJolt/runtimepanel.py
+++ pyJolt/runtimepanel.py
@@ -3,3 +3,26 @@ import wx
 class RuntimePanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
+
+        self.testData = [
+            10, 20, 30, 40, 50, 40, 30, 20, 10
+        ]
+
+        self.Bind(wx.EVT_PAINT, self.onPaint)
+
+    def onPaint(self, event):
+        dc = wx.PaintDC(self)
+
+        dc.SetBackground(wx.Brush(wx.BLACK))
+        dc.Clear()
+
+        dc.SetPen(wx.Pen(wx.RED, 1))
+
+        dc.DrawLine(0,0,30,20)
+
+        dc.SetPen(wx.Pen(wx.BLUE, 1))
+        for i in range(0, len(self.testData)-1):
+            dc.DrawLine(
+                i*10, self.testData[i]*10,
+                (i+1)*10, self.testData[i+1]*10)
+
